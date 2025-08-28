7b2d34b64945854406e3e0303e6ff92177bffb0e
Fumika Mikami <fumicha.3fan1@gmail.com>
2025-08-28 23:44:14 +0100

    fixed all design

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,9 @@ import { useState, useEffect, useRef } from "react";
 import { FaCloud, FaSearch, FaWind, FaCloudRain, FaListUl, FaMap, FaSlidersH, FaSnowflake, FaBolt } from 'react-icons/fa';
 import { IoSunny } from "react-icons/io5";
 import { BsFillCloudFogFill } from "react-icons/bs";
+import { BsFillSunsetFill } from "react-icons/bs";
+import { BsFillSunriseFill } from "react-icons/bs";
+
 
 // Map Open-Meteo weather codes to icons
 function getWeatherIcon(code, size = "text-[3rem]") {
@@ -83,7 +86,7 @@ export default function Homepage() {
     <div className="flex flex-row h-screen">
       {/* LEFT PANEL */}
       <div className=" w-1/7 ">
-        <div className="mb-5 flex flex-col mx-5 px-6 py-6 space-y-20 justify-center items-center bg-gray-800/60 rounded-[20px] my-5 h-[95%]">
+        <div className="mb-5 flex flex-col mx-5 px-6 py-6 space-y-20 justify-center items-center  bg-[#162f4c] rounded-[20px] my-5 h-[95%]">
           <div className="text-gray-500 flex flex-col items-center justify-center gap-3"><FaCloudRain className="text-3xl" />Weather</div>
           <div className="text-gray-500 flex flex-col items-center justify-center gap-3"><FaListUl className="text-3xl" />Cities</div>
           <div className="text-gray-500 flex flex-col items-center justify-center gap-3"><FaMap className="text-3xl" />Map</div>
@@ -94,10 +97,10 @@ export default function Homepage() {
       {/* MAIN CONTENT */}
       <div className=" md:w-4/6 sm:w-10/12">
         {/* Search Bar */}
-        <div className="relative flex items-center m-5">
+        <div className="relative flex items-center mt-3">
           <FaSearch className="absolute left-5 text-gray-500" />
           <input
-            className="h-[40px] bg-gray-800/60 w-full pl-12 pr-5 rounded-xl border border-gray-300 focus:outline-none"
+            className="h-[40px] bg-gray-800/60 w-full pl-12 pr-5 rounded-xl  border-gray-500/20 focus:outline-none"
             type="text"
             placeholder="Enter the city"
             value={city}
@@ -108,15 +111,15 @@ export default function Homepage() {
         {/* WEATHER PANEL */}
         {weather && current && (
           <>
-            <div className="mx-9 justify-between items-center flex flex-row my-5">
-              <div className="flex flex-col space-y-5">
+            <div className="mx-9 justify-between items-center flex flex-row ">
+              <div className="flex flex-col space-y-4">
                 <div>
-                  <div className="font-bold text-4xl">{weather.name}</div>
+                  <div className="font-bold text-3xl">{weather.name}</div>
                   <div className="text-gray-500 text-[18px]">{new Date().toLocaleString()}</div>
                 </div>
                 <div className="flex items-center flex-row space-x-10 p-0">
                   <div className="flex flex-col justify-center items-center ">
-                    <div className="text-8xl font-bold ">{Math.round(current.temperature)}°</div>
+                    <div className="text-7xl font-bold ">{Math.round(current.temperature)}°</div>
                     <div className="text-md text-gray-200 mt-3 gap-2 flex flex-row text-[18px]">
                       <span>High : {daily[0]?.max}</span>
                       <span>Low : {daily[0]?.min}</span>
@@ -133,7 +136,7 @@ export default function Homepage() {
             </div>
 
             {/* Today's Forecast */}
-            <div className="mb-5 bg-gray-800/60 rounded-[20px] flex flex-col mx-5 px-6 py-4 space-y-5">
+            <div className="mb-5  bg-[#162f4c] rounded-[20px] flex flex-col mx-5 px-6 py-2 space-y-3">
               <p className="text-gray-500 font-bold uppercase">Today's Forecast</p>
               <div className="flex items-center justify-between overflow-x-scroll scrollbar-hide">
                 {weather.hourly?.temperature_2m?.slice(0, 6).map((temp, index) => (
@@ -148,48 +151,56 @@ export default function Homepage() {
               </div>
             </div>
             
-            <div className="flex flex-row mx-5 gap-5  rounded-[20px] bg-gray-800/60">
+            <div className="flex flex-row mx-5 gap-5  rounded-[20px]  bg-[#162f4c]">
               {/* Today' Highlight */}
-              <div className="mb-5 w-[65%] space-y-5  flex flex-col px-6 py-4">
+              <div className=" w-[60%] space-y-5  flex flex-col  py-4">
                 <p className="text-gray-500 font-bold uppercase">Today Highlight</p>
-                <div className="flex flex-row">
+
+                <div className="flex flex-col w-full  border-blue-700  items-center space-y-20">
+                {/* 1st column */}
+                <div className="flex flex-row w-full  border-red-900">
+                  {/* 1st column */}
                   <div className="flex-row flex gap-2 w-1/2">
-                    <span><FaWind /></span>
+                    <span><FaWind className="text-[30px]"/></span>
                     <span className="flex flex-col">
                       <div>Humidity: {weather.hourly?.relative_humidity_2m?.[0] ?? "-"}%</div>
                     </span>
                   </div>
+                  {/* second column */}
                   <div className="flex-row flex gap-2 w-1/2">
-                    <span><FaWind /></span>
+                    <span><FaWind className="text-[30px]" /></span>
                     <span className="flex flex-col">
                       <div>Wind Speed</div>
                       <div>{Math.round(current.windspeed)} km/h</div>
                     </span>
                   </div>
                 </div>
-                <div className="flex flex-row">
-                  <div className="flex-row flex gap-2 w-1/2">
-                    <span><FaWind /></span>
+                {/* second column */}
+                <div className="flex flex-row w-full   border-red-90">
+                  <div className="flex-row flex gap-2 w-1/2 border-yellow-200">
+                    <span><FaWind className="text-[30px]"/></span>
                     <span className="flex flex-col">
                       <span>UV Index: {daily[0]?.uvIndex ?? "-"}</span>
                     </span>
                   </div>
                   <div className="flex-row flex gap-2 w-1/2">
-                    <span><FaWind /></span>
+                    <span><FaWind className="text-[30px]"/></span>
                     <span className="flex flex-col">
                       <span>Chance of Rain</span>
                       <span>{daily[0]?.rainChance ?? "-"}%</span>
                     </span>
+                    </div>
                   </div>
                 </div>
               </div>
               
               {/* Sunrise/Sunset/Length of Day */}
-              <div className="mb-5 bg-blue-600/20 space-y-5 rounded-[20px] flex flex-col my-4 px-12 py-4">
-                <div className="text-gray-500 font-bold uppercase">Sunrise</div>
+              <div className="mb-5  bg-[#141b32] space-y-5 rounded-[20px] flex flex-col my-4 px-6 items-center justify-center py-4">
+                <div className="text-gray-500 font-bold uppercase flex items-center gap-2"> <BsFillSunriseFill className="text-2xl"/>Sunrise</div>
                 <div className="text-xl">{daily[0]?.sunrise ? new Date(daily[0].sunrise).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true }).toUpperCase() : "-"}</div>
 
-                <div className="text-gray-500 font-bold uppercase">Sunset</div>
+                 <div className="text-gray-500 font-bold uppercase flex items-center gap-2"><BsFillSunsetFill className="text-2xl"/>
+                 Sunset</div>
                 <div className="text-xl">{daily[0]?.sunset ? new Date(daily[0].sunset).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true }).toUpperCase(): "-"}</div>
 
                 <div className="text-gray-500 font-bold uppercase">Length of day</div>
@@ -203,7 +214,7 @@ export default function Homepage() {
 
       {/* RIGHT PANEL */}
       <div className="  w-2/6 md:block hidden">
-        <div className="mb-5 bg-gray-800/60 mt-10 rounded-[20px] flex flex-col mx-5 px-6 py-5 space-y-5">
+        <div className="mb-5  bg-[#162f4c] mt-5 rounded-[20px] flex flex-col mx-5 px-6 py-3 space-y-3">
           <div className="text-gray-500 font-bold uppercase">7 Day Forecast</div>
           {daily.length > 0 ? (
             <div className="space-y-10">
